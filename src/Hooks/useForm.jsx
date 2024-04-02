ab27e559c5d1ef9d865375d27900d62539f986d7
kevinwduran <kevinwduran23@gmail.com>
2024-04-02 17:24:11 -0300

    Adição Login

---
+++ src/Hooks/useForm.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const types = {
+  email: {
+    regex:
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+    message: "Preencha um e-mail válido.",
+  },
+};
+
+const useForm = (type) => {
+  const [value, setValue] = React.useState("");
+  const [error, setError] = React.useState("");
+
+  function onChange({ target }) {
+    if (error) validate(target.value)
+    setValue(target.value);
+  }
+
+  function validate(value) {
+    if (type === false) return true; // não tem validação
+    if (value.length === 0) {
+      setError("Preencha um valor.");
+      return false;
+    } else if (types[type] && !types[type].regex.test(value)) {
+      setError(types[type].message);
+      return false;
+    } else {
+      return true;
+      setError(null);
+    }
+  }
+
+  return {
+    value,
+    setValue,
+    onChange,
+    error,
+    validate: () => validate(value), // já exporta validando
+    onBlur: () => validate(value),
+  };
+};
+
+export default useForm;
