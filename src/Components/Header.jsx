0221f817bd32895cc2af6a9e87b633cb4b68dec3
kevinwduran <kevinwduran23@gmail.com>
2024-04-03 23:19:15 -0300

    Implementação UserContext para Login e Autentificação

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
-import  Dogs from "../Assets/dogs.svg?react";
+import Dogs from "../Assets/dogs.svg?react";
+import { UserContext } from "../UserContext";
 
 const Header = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`container ${styles.nav}`}>
         <Link to="/" className={styles.logo} aria-label="Dogs - Home">
           <Dogs />
         </Link>
-        <Link to="/login" className={styles.login}>Login / Criar</Link>
+        {data ? (
+          <Link to="/conta" className={styles.login}>
+            {data.nome}
+          </Link>
+        ) : (
+          <Link to="/login" className={styles.login}>
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
